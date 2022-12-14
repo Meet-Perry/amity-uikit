531554cde9b8108e7cf7e5f4dfdc3d7c5a56b8d8
Anton Moiseev <antonm@hey.com>
2022-12-14 17:04:44 +0400

    Sanitize HTML before rendering

--- src/core/components/LinkifyMarkdown/index.js
+++ src/core/components/LinkifyMarkdown/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import sanitize from 'sanitize-html';
 
 const MARKDOWN_LINK_REGEX = /\[([^\\[]+)\]\(?(\S*)?\)/gm;
 
 const Markdown = ({ children }) => {
   const content = children.replace(MARKDOWN_LINK_REGEX, '<a href="$2">$1</a>');
-  return <span dangerouslySetInnerHTML={{ __html: content }} />; // eslint-disable-line react/no-danger
+  return <span dangerouslySetInnerHTML={{ __html: sanitize(content) }} />; // eslint-disable-line react/no-danger
 };
 
 export default Markdown;
